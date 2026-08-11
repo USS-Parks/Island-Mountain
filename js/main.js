3d8152c505342ff3d0416f8d89e2067960a27464
Basho Parks <basho.parks@gmail.com>
2026-08-10 18:13:08 -0700

    Install LinkedIn Insight Tag site-wide (partner 9789228) via main.js plus 3 pages without it
    Authored and reviewed by Bashō Parks, copyright 2026

--- js/main.js
+++ js/main.js
@@ -152,3 +152,21 @@
   });
 
 })();
+
+/* LinkedIn Insight Tag (partner 9789228) - loaded site-wide via this shared script. */
+(function () {
+  window._linkedin_partner_id = "9789228";
+  window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+  window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
+  (function (l) {
+    if (!l) {
+      window.lintrk = function (a, b) { window.lintrk.q.push([a, b]); };
+      window.lintrk.q = [];
+    }
+    var s = document.getElementsByTagName("script")[0];
+    var b = document.createElement("script");
+    b.type = "text/javascript"; b.async = true;
+    b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+    s.parentNode.insertBefore(b, s);
+  })(window.lintrk);
+})();
